8547232fdf539a8767001cbf96f685c44283e84a
Rohit Gupta <93462249+Guptarohit2003@users.noreply.github.com>
2024-03-12 23:11:03 +0530

    12 March Problem

---
+++ Daily Problems/12-03-Remove-Zero-Sum-Consecutive-Nodes-from-Linked-List.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        prefix = 0
+        seen = {0: dummy}
+        while head:
+            prefix += head.val
+            seen[prefix] = head
+            head = head.next
+        head = dummy
+        prefix = 0
+        while head:
+            prefix += head.val
+            head.next = seen[prefix].next
+            head = head.next
+        return dummy.next
